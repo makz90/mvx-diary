b8ffd263f366e3a663dd05d9af779da91e5e6e6e
krawm <maksym.krawczyk@autodesk.com>
2017-08-28 15:55:08 +0200

    iOS project added and started

---
+++ MvxDiary.UI.iOS/Setup.cs
@@ -0,0 +1,20 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.iOS.Platform;
+using MvvmCross.iOS.Views.Presenters;
+using MvxDiary.Core;
+
+namespace MvxDiary.UI.iOS
+{
+    class Setup : MvxIosSetup
+    {
+        public Setup(IMvxApplicationDelegate applicationDelegate, IMvxIosViewPresenter presenter) : base(applicationDelegate, presenter)
+        {
+
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            return new App();
+        }
+    }
+}
---
+++ MvxDiary.UI.iOS/Views/AddDiaryItemView.cs
@@ -0,0 +1,27 @@
+﻿using MvvmCross.Binding.BindingContext;
+using MvvmCross.iOS.Views;
+using MvxDiary.Core.ViewModels;
+
+namespace MvxDiary.UI.iOS.Views
+{
+    public partial class AddDiaryItemView : MvxViewController<AddDiaryItemViewModel>
+    {
+        public AddDiaryItemView() : base("AddDiaryItemView", null)
+        {
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            this.CreateBinding(DateLabel).To((AddDiaryItemViewModel vm) => vm.Date).Apply();
+            this.CreateBinding(TitleEdit).To((AddDiaryItemViewModel vm) => vm.Title).Apply();
+            this.CreateBinding(DescriptionEdit).To((AddDiaryItemViewModel vm) => vm.Description).Apply();
+        }
+    }
+}
